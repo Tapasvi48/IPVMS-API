46548f3b81d33ffcf4f7d748ece8f39482abbb1f
TAPASVI <Tapasviarora2002@gmail.com>
2024-05-30 12:50:15 +0530

    added notifications

--- src/utils/notification.config.js
+++ src/utils/notification.config.js
@@ -4,6 +4,7 @@ export const entityTypeActions = (user_name, entity_name) => {
     202: `letter is signed by ${user_name}`,
     301: `${entity_name} is approved by ${user_name}`,
     302: `${entity_name} is rejected by ${user_name}`,
+    303: `${entity_name} is send by ${user_name} for approval`,
   };
 };
 
@@ -12,4 +13,5 @@ export const entityTypeIdMapping = {
   REJECTED: "302",
   LETTER_SENT: "201",
   LETTER_SIGNED: "202",
+  SEND_APPROVAL: "303",
 };
